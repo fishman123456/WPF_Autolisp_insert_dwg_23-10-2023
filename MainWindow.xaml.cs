dd9075ba30d2a44ee8591dbb9ef148e78b6f99a6
denis <106389581+fishman123456@users.noreply.github.com>
2023-10-23 18:44:50 +0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Autolisp_insert_dwg_23_10_2023
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        // метод для записи в файл lisp
+        private void Button_Add_Number_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string sbor = Number_String.file_block_ins() + TextBox_increment_name.Text + Number_String.file_block_ins_two();
+                File.WriteAllText(saveFileDialog.FileName, sbor);
+                saveFileDialog.InitialDirectory = @"F:\Проекты\_Чегдомын отделение флотации Главный корпус";
+                saveFileDialog.Filter = "Text file (*.lsp)|*.lsp";
+            }
+
+
+            // метод для создания файла
+
+        }
+
+    }
+}
---
+++ Number_String.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Office.Interop.Excel;
+using Microsoft.Win32;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_Autolisp_insert_dwg_23_10_2023
+{
+    public static class Number_String
+    // строка для записи в файл
+    {
+        static string file_block_ins_str = null;
+        static string file_block_ins_str_two = null;
+        static string file_block_ins_str_three = null;
+        public static void CheckDate()
+        {
+            DateTime dt1 = DateTime.Now;
+            DateTime dt2 = DateTime.Parse("01/01/2024");
+            System.Windows.Window w1 = new System.Windows.Window();
+
+            if (dt1.Date > dt2.Date)
+            {
+                MessageBox.Show("Your Application is Expire");
+                // Выход из проложения добавил 12-07-2023. Чтобы порядок был....
+                System.Windows.Application.Current.Shutdown();
+                //w1.Close();
+            }
+            else
+            {
+
+                MessageBox.Show("Работайте до   " + dt2.ToString());
+            }
+
+        }
+
+        public static string file_block_ins()
+        {
+            file_block_ins_str += "\n (defun C: u_83( / dic edic)\n";
+            file_block_ins_str += "(vl - load - com)\n";
+            file_block_ins_str += "(command \"_ - INSERT\"\n";
+            file_block_ins_str += ";c:\\Program Files\\AutoCAD 2009\\AcadLsp\\St_prof\\Block\\001.dwg - пример\n";
+            file_block_ins_str += "F:\\Проекты\\_Чегдомын отделение флотации Главный корпус\\ГЛАВНЫЙ КОРПУС\\ТХ - 6 листы в модели\\Л.\n";   
+            return file_block_ins_str;
+        }
+        public static string file_block_ins_two()
+
+        {
+            file_block_ins_str_two += "_La.dwg\n";
+            file_block_ins_str_two += @"(getpoint)\n";
+            file_block_ins_str_two += "\"1\"; масштаб по оси X\n";
+            file_block_ins_str_two += "\"1\"; масштаб по оси Y\n";
+            file_block_ins_str_two += "\"0\"; угол поворота\n";
+            file_block_ins_str_two += ")\n";
+            file_block_ins_str_two += "(princ)\n";
+            file_block_ins_str_two += ")\n";
+            return file_block_ins_str_two;
+        }
+    }
+
+
+}
