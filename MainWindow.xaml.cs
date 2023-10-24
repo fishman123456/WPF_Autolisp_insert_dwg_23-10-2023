ce533c4b7e9076172cbceb7ab7b394e3a7683805
denis <106389581+fishman123456@users.noreply.github.com>
2023-10-24 11:41:09 +0700

    собираем lisp  для вставки ссылок

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -33,19 +33,30 @@ namespace WPF_Autolisp_insert_dwg_23_10_2023
         // метод для записи в файл lisp
         private void Button_Add_Number_Click(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                string sbor = Number_String.file_block_ins() + Number_String.file_block_ins_one +
-                    TextBox_increment_name.Text + Number_String.file_block_ins_two() + Number_String.file_block_ins_three();
-                File.WriteAllText(saveFileDialog.FileName, sbor);
-                saveFileDialog.InitialDirectory = @"F:\Проекты\_Чегдомын отделение флотации Главный корпус";
-                saveFileDialog.Filter = "Text file (*.lsp)|*.lsp";
-            }
 
 
-            // метод для создания файла
+            string sbor = $"{Number_String.file_block_ins()}{Number_String.file_block_ins_one()}" +
+                $"{TextBox_increment_name.Text}{Number_String.file_block_ins_two()}" +
+                $"{Number_String.file_block_ins_three()}";
 
+
+            // диалог сохранения файла lisp
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "LSP Files(*.lsp)|*.lsp|All(*.*)|*";
+            dialog.RestoreDirectory = true;
+            dialog.InitialDirectory = dialog.FileName;
+                //SaveFileDialog dialog = new SaveFileDialog();
+               
+                    if (dialog.ShowDialog() == true)
+                    {
+                        string path = dialog.FileName;
+                        StreamWriter sw = new StreamWriter(path, false);
+                        using (sw)
+                        {
+
+                            sw.Write(sbor);
+                        }
+                    }
         }
 
     }
--- Number_String.cs
+++ Number_String.cs
@@ -13,13 +13,13 @@ using System.Windows;
 
 namespace WPF_Autolisp_insert_dwg_23_10_2023
 {
-    public static class Number_String
+    public class Number_String
     // строка для записи в файл
     {
-        static string file_block_ins_str = null;
-        static string file_block_ins_str_one = null;
-        static string file_block_ins_str_two = null;
-        static string file_block_ins_str_three = null;
+        private static string file_block_ins_str = "";
+        private static string file_block_ins_str_one = "";
+        private static string file_block_ins_str_two = "";
+        private static string file_block_ins_str_three = "";
         public static void CheckDate()
         {
             DateTime dt1 = DateTime.Now;
@@ -41,35 +41,41 @@ namespace WPF_Autolisp_insert_dwg_23_10_2023
 
         }
 
+        // собираем строки для файла lisp
         public static string file_block_ins()
         {
-            file_block_ins_str += "\n (defun C: u_83( / dic edic)\n";
-            file_block_ins_str += "(vl - load - com)\n";
+            file_block_ins_str += "\n (defun C: u_83( / dic edic)";
+            file_block_ins_str += "\n(vl-load-com)";
             return file_block_ins_str;
         }
         // первую и вторую надо зациклить по кол-ву чисел в текстбоксе
         public static string file_block_ins_one()
         {
-            file_block_ins_str_one += "(command \"_ - INSERT\"\n";
-            file_block_ins_str_one += @";c:\\Program Files\\AutoCAD 2009\\AcadLsp\\St_prof\\Block\\001.dwg - пример\n";
-            file_block_ins_str_one += "F:\\Проекты\\_Чегдомын отделение флотации Главный корпус\\ГЛАВНЫЙ КОРПУС\\ТХ - 6 листы в модели\\Л.\n";
+            file_block_ins_str_one += @";c:\Program Files\\AutoCAD 2009\AcadLsp\St_prof\Block\001.dwg - пример" + "\n";
+            file_block_ins_str_one += "\n;начало блока"+"\n";
+
+            file_block_ins_str_one += "(command \"_.xref\" \"\" \"";
+            file_block_ins_str_one += @"F:\Проекты\_Чегдомын отделение флотации Главный корпус\ГЛАВНЫЙ КОРПУС\ТХ - 6 листы в модели\" ;
             return file_block_ins_str_one;
         }
+        // для справки
+        //  (command "_.xref" "" "F:\\Проекты\\_Чегдомын отделение флотации Главный корпус\\ГЛАВНЫЙ КОРПУС\\ТХ-6 листы в модели\\2.dwg" "0,0,0" "" "" "")
+        // после этой строчки вставляем данные из textboxa
+
+        // продолжаем далее....
         public static string file_block_ins_two()
         {
-            file_block_ins_str_two += "_La.dwg\n";
-            file_block_ins_str_two += @"(getpoint)\n";
-            file_block_ins_str_two += "\"1\"; масштаб по оси X\n";
-            file_block_ins_str_two += "\"1\"; масштаб по оси Y\n";
-            file_block_ins_str_two += "\"0\"; угол поворота\n";
+            file_block_ins_str_two += ".dwg\" ";
+           // file_block_ins_str_two += "\n(getpoint)\n";
+            file_block_ins_str_two += "\"0,0,0\" \"\" \"\" \"\""; //координаты вставки\n";
             file_block_ins_str_two += ")\n";
 
             return file_block_ins_str_two;
         }
         public static string file_block_ins_three()
         {
-            file_block_ins_str_two += "(princ)\n";
-            file_block_ins_str_two += ")\n";
+            file_block_ins_str_three += "(princ)\n";
+            file_block_ins_str_three += ")\n";
             return file_block_ins_str_three;
         }
     }
